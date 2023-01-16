bdca4814d33649081f7fbf2a25a639597db19135
Tim de Jager <tim@prefix.dev>
2023-01-16 16:25:42 +0100

    fix: more wasm fixes

--- src/lib.rs
+++ src/lib.rs
@@ -61,41 +61,88 @@ impl Default for SvgOptions {
 }
 
 
+pub struct CircleOutput {
+    pub x: u32,
+    pub y: u32,
+    pub radius: u32,
+    pub hue: u16,
+}
+
+pub struct RectOutput {
+    pub x: u32,
+    pub y: u32,
+    pub rotation: u8,
+    pub size: u32,
+    pub stroke_width: u32,
+    pub hue: u16,
+    pub stroke_hue: u16,
+}
+
+pub struct SVGData {
+    pub circle: CircleOutput,
+    pub rect: RectOutput,
+    pub viewbox: (u32, u32)
+}
+
+
 /// Generate the Pricom image
-pub fn generate<S: Into<String>>(options: SvgOptions, input: S) -> SVG {
+fn generate_data<S: Into<String>>(input: S, options: SvgOptions) -> SVGData {
 
     // Hash the string
     let mut hasher = Sha256::new();
     hasher.update(input.into().as_bytes());
     let result = hasher.finalize();
-
-    // TODO make this generic for non-square sizes
     let rect_size = generate_rect_size(&result) as u32;
-    let circle_pos = options.width / 2;
-    let circle_r = options.width / 2;
-    let rect_pos = circle_pos - rect_size / 2;
-    let rotation = format!("rotate({} {} {})", generate_rotation_angle(&result[1..]), circle_pos, circle_pos);
-    let circle_fill = format!("hsl({} 80% 80%)", generate_hue(&result[2..]));
-    let rect_fill = format!("hsl({} 80% 30%)", generate_hue(&result[4..]));
-    let stroke = format!("hsl({} 80% 30%)", generate_hue(&result[6..]));
+
+    let circle = CircleOutput {
+        x: options.width / 2,
+        y: options.height / 2,
+        radius: options.width / 2,
+        hue: generate_hue(&result[2..]),
+    };
+
+    let rect = RectOutput {
+        x: circle.x - rect_size / 2,
+        y: circle.y - rect_size / 2,
+        rotation: generate_rotation_angle(&result[1..]),
+        size: rect_size,
+        stroke_width: options.outer_rect_stroke_width,
+        hue: generate_hue(&result[4..]),
+        stroke_hue: generate_hue(&result[6..])
+    };
+
+    SVGData {
+        circle,
+        rect,
+        viewbox: (options.width, options.height)
+    }
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+pub fn generate<S: Into<String>>(input: S, options: SvgOptions) -> SVG {
+    let data = generate_data(input, options);
+    let rotation = format!("rotate({} {} {})", data.rect.rotation, data.circle.x, data.circle.y);
+    let circle_fill = format!("hsl({} 80% 80%)", data.circle.hue);
+    let rect_fill = format!("hsl({} 80% 30%)", data.rect.hue);
+    let stroke = format!("hsl({} 80% 30%)", data.rect.stroke_hue);
 
     let circle = Circle::new()
-        .set("cx", circle_pos)
-        .set("cy",circle_pos)
-        .set("r", circle_r)
+        .set("cx", data.circle.x)
+        .set("cy",data.circle.y)
+        .set("r", data.circle.radius)
         .set("fill", circle_fill);
     let rect = Rectangle::new()
-        .set("width", rect_size)
-        .set("height", rect_size)
+        .set("width", data.rect.size)
+        .set("height", data.rect.size)
         .set("fill", rect_fill)
         .set("transform", rotation)
         .set("stroke", stroke)
-        .set("stroke-width", 3)
-        .set("x", rect_pos)
-        .set("y", rect_pos);
+        .set("stroke-width", data.rect.stroke_width)
+        .set("x", data.rect.x)
+        .set("y", data.rect.y);
 
     let document = Document::new()
-        .set("viewBox", (0, 0, options.width, options.height))
+        .set("viewBox", (0, 0, data.viewbox.0, data.viewbox.1))
         .add(circle)
         .add(rect);
 
@@ -107,6 +154,6 @@ pub fn generate<S: Into<String>>(options: SvgOptions, input: S) -> SVG {
 
 #[cfg(target_arch = "wasm32")]
 #[wasm_bindgen]
-pub fn generate_web(name: &str, options: Option<SvgOptions>) {
-    crate::generate(options.unwrap_or_default(), name);
+pub fn generate(name: &str, options: Option<SvgOptions>) {
+    crate::generate_data(name, options.unwrap_or_default());
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use pricon::SvgOptions;
 
-
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
-
     let name = "wolfv/mamba";
-    let svg = pricon::generate(SvgOptions::default(), name);
+    let svg = pricon::generate(name, SvgOptions::default());
     svg::save("image.svg", &svg).unwrap();
+}
+
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
 }
